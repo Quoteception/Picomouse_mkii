8789d19fb4dc0577743ed3bc6a89099fcc982ac6
Montell Higgins-Chen <57107547+Quoteception@users.noreply.github.com>
2022-08-01 20:01:29 +0100

    Create MakerPi2040.ino

---
+++ MakerPi2040.ino
@@ -0,0 +1,15 @@
+#include <Adafruit_GFX.h>
+#include <Adafruit_ST7735.h>
+#include <Adafruit_ST7789.h>
+#include <Adafruit_ST77xx.h>
+
+//Pin Mapping
+//LCD TFT Display = GPIO 0/1 (Grove 1) + GPIO 2/3 (Grove 2)
+//Touch bar sensor = GPIO 4/5 (Grove 3)
+//IR Sensor = GPIO 16/17 (Grove 4)
+//Ultrasonic Sensor = GPIO14/15 
+//Tachometers = GPIO 7/28 (Grove 7)
+//DC Motors = GPIO 8/9/10/11
+//RGB LEDs = GPIO 18
+//User Programmable Buttons = GPIO 20/21
+//Piezo Buzzer = GPIO 22
